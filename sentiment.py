e35e736856667cafc7971b2ca5053b3109ccc54a
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-06-05 20:26:46 +0530

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -1,9 +1,8 @@
-# sentiment.py
 from transformers import pipeline
 
-# Use a pre-trained sentiment-analysis pipeline
 sentiment_analyzer = pipeline("sentiment-analysis")
 
 def get_sentiment(text):
     result = sentiment_analyzer(text)[0]
-    return {"label": result['label'].lower(), "score": result['score']}
+    return {"label": result["label"].lower(), "score": result["score"]}
+
